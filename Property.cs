7fe610200374122aec53f0c2f72a71f2f8deabfb
webbgamers <webbgamers@gmail.com>
2021-02-21 20:46:15 -0500

    use dynamic for property value

--- Property.cs
+++ Property.cs
@@ -3,10 +3,10 @@ namespace IIoWParser
     public class Property
     {
         public string id { get; set; }
-        public object value { get; set; }
+        public dynamic value { get; set; }
         public int layer { get; set; }
 
-        public Property(string id, object value, int layer)
+        public Property(string id, dynamic value, int layer)
         {
             this.id = id;
             this.value = value;
